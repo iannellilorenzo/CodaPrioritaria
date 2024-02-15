2ca6803b75809b3f419656a85b56c21ce0227485
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-02-15 10:21:24 +0100

    Aggiungere i file di progetto.

---
+++ CodaPrioritaria/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class CodaPrioritaria
+{
+    private ArrayList _arrList;
+
+    public ArrayList ArrList
+    { 
+        get => _arrList;
+        set => _arrList = value;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
